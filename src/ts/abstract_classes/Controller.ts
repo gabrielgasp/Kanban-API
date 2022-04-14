cfca1bc170c88b285f00cddb25c47c04f6ddc655
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-14 20:07:08 -0300

    doc: add an explanation about controller layer in Controller.ts

--- src/ts/abstract_classes/Controller.ts
+++ src/ts/abstract_classes/Controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express'
 import { IController, IService } from '../interfaces'
 
+// Controller is where we handle everything related to http requests.
+// It is responsible to call the service and return the correct status code and data
+// to the client according to the result of the operation.
+// The controller has no knowledge of the database or business logic behind your application.
 export abstract class Controller<T> implements IController {
   constructor (
     protected service: IService<T>
