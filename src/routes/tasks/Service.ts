f955bd2742f202c2064729812d2da33852e90540
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-14 19:27:29 -0300

    feat: create TasksService extending AbstractService and create method tests

---
+++ src/routes/tasks/Service.ts
@@ -0,0 +1,4 @@
+import { AbstractService } from '../../ts/abstract_classes'
+import { ITask } from '../../ts/interfaces'
+
+export class TasksService extends AbstractService<ITask> {}
---
+++ tests/unit/routes/tasks/service/create.spec.ts
@@ -0,0 +1,32 @@
+import { TasksService } from "../../../../../src/routes/tasks/Service"
+import { MockTasksRepository } from "../../../../__mocks__"
+
+const mockTasksRepository = new MockTasksRepository()
+const taskService = new TasksService(mockTasksRepository)
+
+const fakeTask = {
+  boardId: 1,
+  status: 'todo',
+  title: 'test',
+  description: 'test',
+  members: ['test'],
+  tags: ['test']
+}
+
+describe("TasksService create method unit tests", () => {
+  beforeAll(() => { // mock the return value of the taskRepository.create method
+    mockTasksRepository.create = jest.fn().mockResolvedValue({ ...fakeTask, _id: 'test' })
+  })
+
+  it("should call the create method of the repository with data received as argument", async () => {
+    await taskService.create(fakeTask)
+
+    expect(mockTasksRepository.create).toHaveBeenCalledWith(fakeTask)
+  })
+
+  it("should return the result of the repository's create method", async () => {
+    const result = await taskService.create(fakeTask)
+
+    expect(result).toEqual({ ...fakeTask, _id: 'test' })
+  })
+})
