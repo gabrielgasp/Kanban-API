675b2a32131328f6341527fc78ad0970bc47d4c4
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-14 19:15:13 -0300

    test: create MockTasksRepository to use in service tests

---
+++ tests/__mocks__/unit/repositories/MockTasksRepository.ts
@@ -0,0 +1,15 @@
+import { TasksRepository } from "../../../../src/repositories/TasksRepository";
+
+// This is a mock of the TasksRepository that we can use during tests to
+// create an instance of the TasksService and test its methods.
+
+export class MockTasksRepository extends TasksRepository {
+  constructor () {
+    super({} as any) // I don't need to pass the real tasksModel because we will not use it
+  }
+
+  // Here I override all methods from the original TasksRepository class
+  // and turn them into empty mocks.
+
+  public create = jest.fn()
+}
