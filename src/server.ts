64a93f5e1573a06c38271ee173eaa76d3f6a91ee
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-14 10:35:52 -0300

    feat: add express app.listen to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import { connectToDatabase } from './database'
+import { app } from './app'
+
+const PORT = process.env.PORT ?? 3001
 
 const start = async (): Promise<void> => {
   await connectToDatabase()
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
 }
 
 void start()
