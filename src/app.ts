dcb53243c9f82f85aefb18cc254c7cd991ac7cf3
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-14 11:04:30 -0300

    feat: add errorHandler middleware to express app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config'
 import express, { Express } from 'express'
-import 'express-async-errors' // This is a lib that will automatically catch all async errors
+import 'express-async-errors' // This is a lib that will automatically catch all async errors and send them to the error handler
+import { errorHandler } from './middlewares'
 
 const app: Express = express() // Initialize express app
 
 app.use(express.json()) // Parse all JSON in incoming requests so they can be used as JS objects
+
+app.use(errorHandler) // Receive errors when next(error) is called
 
 export { app } // Export the app so it can be used in tests and the in the server.ts file
