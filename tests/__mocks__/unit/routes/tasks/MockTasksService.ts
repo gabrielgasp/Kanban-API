f4eb3a2d6671001c480cf6440337c6ebee506120
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-14 19:46:01 -0300

    test: create MockTasksService to use in controller tests

---
+++ tests/__mocks__/unit/routes/tasks/MockTasksService.ts
@@ -0,0 +1,18 @@
+import { TasksService } from "../../../../../src/routes/tasks/Service"
+
+// This is a mock of the TasksService that we can use during tests to
+// create an instance of the TasksController and test its methods.
+
+export class MockTasksService extends TasksService {
+  constructor () {
+    super({} as any) // I don't need to pass the real TasksRepository because we will not use it
+  }
+
+  // Here I override all methods from the original TasksService class
+  // and turn them into empty mocks.
+
+  public create = jest.fn()
+  public read = jest.fn()
+  public update = jest.fn()
+  public delete = jest.fn()
+}
