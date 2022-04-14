cbf2041fb597e839f233ac33b3e6021908aa675d
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-14 20:01:39 -0300

    doc: add comments in routes/tasks/Service.ts

--- src/routes/tasks/Service.ts
+++ src/routes/tasks/Service.ts
@@ -1,4 +1,8 @@
 import { AbstractService } from '../../ts/abstract_classes'
 import { ITask } from '../../ts/interfaces'
 
+// This is the class that will be used to handle business logic for the tasks;
+// It has the same methods as the abstract Service class, but it uses a "TaskRepository" instance provided as argument
+// to the constructor;
+// If needed, we can add additional methods or override the ones from the abstract class (polymorphism).
 export class TasksService extends AbstractService<ITask> {}
