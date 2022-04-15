9f46349b407d90e547b6c24499c17ec7aca3fc05
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-15 20:46:20 -0300

    test: create 'not found' integration tests for PATCH /tasks/:id endpoint

--- tests/integration/tasks/update.test.ts
+++ tests/integration/tasks/update.test.ts
@@ -107,6 +107,13 @@ describe('Tasks Update endpoint integration tests', () => {
   })
 
   describe('When operation fails', () => {
+    it('Should 404 when trying to update a task that does not exist', async () => {
+      const { status, body } = await fetchEndpoint(`${endpoint}/000000000000000000000000`, { method: 'patch', body: { status: 'in_progress' } })
+
+      expect(status).toBe(404)
+      expect(body.message).toBe('Task not found')
+    })
+
     it('Should 400 with message and provided ID is not a valid ObjectId', async () => {
       const { status, body } = await fetchEndpoint(`${endpoint}/123`, { method: 'patch' }) // path id validation happens before body, that's why I'm not passing any body
 
