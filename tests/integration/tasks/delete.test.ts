b9640b5a919c427bd6c6c7c607e0051ffef728e1
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-15 20:53:27 -0300

    test: add invalid objectid integration test to DELETE /tasks/:id

--- tests/integration/tasks/delete.test.ts
+++ tests/integration/tasks/delete.test.ts
@@ -60,5 +60,12 @@ describe('Tasks Delete endpoint integration tests', () => {
       expect(status).toBe(404)
       expect(body.message).toBe('Task not found')
     })
+
+    it('Should 400 with message and provided ID is not a valid ObjectId', async () => {
+      const { status, body } = await fetchEndpoint(`${endpoint}/123`, { method: 'delete' }) // path id validation happens before body, that's why I'm not passing any body
+
+      expect(status).toBe(400)
+      expect(body.message).toBe('ID must be a valid ObjectId' )
+    })
   })
 })
