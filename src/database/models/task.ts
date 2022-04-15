a30d35122073fda7901d519eced44d244e429071
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-15 19:23:21 -0300

    fix: adjust which fields are required by mongoose model

--- src/database/models/task.ts
+++ src/database/models/task.ts
@@ -7,9 +7,9 @@ const taskSchema = new mongoose.Schema<TaskDocument>({
   boardId: { type: Number, required: true }, // This type would probably be changed to ObjectId if we have a "boards" collection
   status: { type: String, required: true }, // This represents the column ex: "BACKLOG" | "IN_PROGRESS" | "DONE", etc.
   title: { type: String, required: true },
-  description: { type: String, required: true },
-  members: { type: [String], required: true }, // This type would probably be changed to ObjectId if we have a "users" collection
-  tags: { type: [String], required: true } // This type would probably be changed to ObjectId if we have a "tags" collection
+  description: String,
+  members: [String], // This type would probably be changed to ObjectId if we have a "users" collection
+  tags: [String] // This type would probably be changed to ObjectId if we have a "tags" collection
 }, {
   timestamps: true,
   collection: 'tasks',
