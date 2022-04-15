04339b3852e26d2a825999fd330abba1aeaaef5f
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-15 17:42:09 -0300

    test: create invalid members integration tests for POST /tasks endpoint

--- tests/integration/tasks/create.test.ts
+++ tests/integration/tasks/create.test.ts
@@ -126,5 +126,26 @@ describe('Tasks Create endpoint integration tests', () => {
       expect(status).toBe(400)
       expect(body.message).toBe('"description" must be a string')
     })
+
+    it('Should 400 with message when members is not an array', async () => {
+      const { status, body } = await fetchEndpoint(endpoint, { method: 'post', body: { ...newTask, members: 'member' } })
+
+      expect(status).toBe(400)
+      expect(body.message).toBe('"members" must be an array')
+    })
+
+    it('Should 400 with message when a members item is not a string', async () => {
+      const { status, body } = await fetchEndpoint(endpoint, { method: 'post', body: { ...newTask, members: [1] } })
+
+      expect(status).toBe(400)
+      expect(body.message).toBe('"members[0]" must be a string')
+    })
+
+    it('Should 400 with message when a members item is an empty string', async () => {
+      const { status, body } = await fetchEndpoint(endpoint, { method: 'post', body: { ...newTask, members: [""] } })
+
+      expect(status).toBe(400)
+      expect(body.message).toBe('"members[0]" can not be an empty string')
+    })
   })
 })
