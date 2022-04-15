7ec47a58d84b1ceab32a105cace7d47c596fa7a5
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-15 17:05:02 -0300

    refactor: simplify mongodb-memory-server setup for integration tests

--- jest.config.ts
+++ jest.config.ts
@@ -97,7 +97,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  preset: '@shelf/jest-mongodb',
+  preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -141,7 +141,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "node", // This will conflict with jest-mongodb preset if enabled
+  testEnvironment: "node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
