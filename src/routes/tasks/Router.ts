376e4a4105c2f1883edb0eb0034213d4e7fc0179
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-15 14:49:19 -0300

    feat: create TasksRouter with create endpoint

---
+++ src/routes/tasks/Router.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express'
+import { Controller } from '../../ts/abstract_classes'
+import { ITask, IMiddlewares, IValidators } from '../../ts/interfaces'
+
+export class TasksRouter {
+  public router = Router()
+
+  constructor (
+    private readonly controller: Controller<ITask>,
+    private readonly middlewares: IMiddlewares,
+    private readonly validators: IValidators
+  ) {
+    this.init()
+  }
+
+  /*
+  About validateBody:
+  validateBody is a function that expects another function as argument (one that follows the TBodyValidator contract).
+  When we call validateBody with said function as argument, it will return a new function that will be used as middleware.
+  The new function will be called by express with the request, response and next, and will validate the body of the request
+  using the function we passed in the first place. This way we can have a generic "validateBody" middleware that can be used
+  for any request that has a body.
+  */
+
+  public init (): void {
+    this.router.post(
+      '/',
+      this.middlewares.validateBody(this.validators.newTaskValidator),
+      this.controller.create
+    )
+  }
+}
