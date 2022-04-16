44780b446d3139b75cfff64433b00a022d3ce50e
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-15 23:02:43 -0300

    test: create test to validate redirect from '/' to '/tasks'

---
+++ tests/integration/rootRedirect.test.ts
@@ -0,0 +1,10 @@
+import { fetchEndpoint } from './__helpers__'
+
+describe('GET to "/"', () => {
+  it('Should 302 with redirect to "/tasks"', async () => {
+    const { status, header } = await fetchEndpoint('/')
+
+    expect(status).toBe(302)
+    expect(header.location).toBe('/tasks')
+  })
+})
