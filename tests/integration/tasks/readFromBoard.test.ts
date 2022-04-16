930cecd9f002a139a79b7d75fb0a083650633cc5
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-16 16:54:52 -0300

    feat: create success case integration test for GET /tasks/board/:boardId

---
+++ tests/integration/tasks/readFromBoard.test.ts
@@ -0,0 +1,58 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+import { taskModel } from '../../../src/database'
+import { fetchEndpoint } from '../__helpers__'
+
+const endpoint = '/tasks/board'
+
+const tasks = [
+  {
+    boardId: 1,
+    status: 'in_progress',
+    title: 'task that I am doing',
+    priority: 2,
+  },
+  {
+    boardId: 1,
+    status: 'in_progress',
+    title: 'task that I am also doing but is more important',
+    priority: 5,
+  },
+  {
+    boardId: 1,
+    status: 'done',
+    title: 'task that I have done',
+    priority: 3,
+  },
+]
+
+describe('Tasks readFromBoard endpoint integration tests', () => {
+  let mongod: MongoMemoryServer;
+
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create() // Here we create the mongoDB instance in memory so we can test without using our database (and it's really fast)
+    const uri = mongod.getUri() // Get the URI of the database we just created
+    await mongoose.connect(uri) // Here we connect to the mongoDB instance in memory so our application can use it
+    await taskModel.insertMany(tasks)
+  })
+  
+  afterAll(async () => {
+    await mongoose.connection.close() // Here we close the connection to the mongoDB instance in memory
+    await mongod.stop() // Here we stop the mongoDB instance in memory
+  })
+
+  describe('When there are tasks in the collection for the boardId provided', () => {
+    it('Should 200 with all tasks ordered by and status and priority', async () => {
+      const { status, body } = await fetchEndpoint(`${endpoint}/1`)
+
+      expect(status).toBe(200)
+      expect(body[0]).toHaveProperty('_id')
+      expect(body[0]).toMatchObject(tasks[2])
+      expect(body[1]).toHaveProperty('_id')
+      expect(body[1]).toMatchObject(tasks[1])
+      expect(body[2]).toHaveProperty('_id')
+      expect(body[2]).toMatchObject(tasks[0])
+    })
+  })
+
+})
