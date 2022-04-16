5e4a47a31fea0f291d6fe6f93fb66f4646876ad6
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-16 15:45:44 -0300

    test: add 'invalid' and 'undefined' operation cases in PATCH /tasks/:id/members integration test

--- tests/integration/tasks/updateMembers.test.ts
+++ tests/integration/tasks/updateMembers.test.ts
@@ -83,5 +83,19 @@ describe('Tasks updateMembers endpoint integration tests', () => {
       expect(status).toBe(400)
       expect(body.message).toBe('ID must be a valid ObjectId' )
     })
+
+    it('Should 400 with message when trying to update without an operation', async () => {
+      const { status, body } = await fetchEndpoint(`${endpoint}/${taskId}/members`, { method: 'patch', body: { operation: undefined, value: 'value' } })
+
+      expect(status).toBe(400)
+      expect(body.message).toBe('"operation" is required')
+    })
+
+    it('Should 400 with message when trying to update with invalid operation', async () => {
+      const { status, body } = await fetchEndpoint(`${endpoint}/${taskId}/members`, { method: 'patch', body: { operation: 'invalid', value: 'value' } })
+
+      expect(status).toBe(400)
+      expect(body.message).toBe('"operation" must be 1 (add) or -1 (remove)')
+    })
   })
 })
