4b0f9f9f8c9f5048e1303629fc99cb5b1e553bfe
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-16 21:04:16 -0300

    feat (no-prod): add example tasks seeding on first start

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose'
+import tasksSeed from './tasksSeed.json'
 
 export const connectToDatabase = async (): Promise<void> => {
   try {
     const mongoURI = process.env.MONGO_URI ?? 'mongodb://localhost:27017/mamboo-kanban-api'
     await mongoose.connect(mongoURI)
     console.log('Database connection established')
+    const db = mongoose.connection
+    if (await db.collection('tasks').countDocuments() === 0) {
+      await db.collection('tasks').insertMany(tasksSeed)
+      console.log('Database seeded')
+    }
   } catch (e) {
     console.error('Database connection failed')
     process.exit(1)
