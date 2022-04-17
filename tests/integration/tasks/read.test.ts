55797fa6b782effd5b34bc787b399822420b80c1
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-17 03:30:19 -0300

    test: add read integration tests for when trying to fetch with page or limit NaN

--- tests/integration/tasks/read.test.ts
+++ tests/integration/tasks/read.test.ts
@@ -169,5 +169,35 @@ describe('Tasks Read endpoint integration tests', () => {
         expect(body.data).toHaveLength(10)
       })
     })
+
+    describe('When trying to fetch with page that can not be parsed into a number', () => {
+      it('Should 200 with response using page 1 as default', async () => {
+        const { status, body } = await fetchEndpoint(endpoint + '?page=a&limit=5')
+
+        expect(status).toBe(200)
+        expect(body.totalDocs).toBe(28)
+        expect(body.docsPerPage).toBe(5)
+        expect(body.totalPages).toBe(6)
+        expect(body.currentPage).toBe(1)
+        expect(body.nextPage).toBe(2)
+        expect(body.previousPage).toBeUndefined()
+        expect(body.data).toHaveLength(5)
+      })
+    })
+
+    describe('When trying to fetch with limit that can not be parsed into a number', () => {
+      it('Should 200 with response using limit 10 as default', async () => {
+        const { status, body } = await fetchEndpoint(endpoint + '?page=1&limit=a')
+
+        expect(status).toBe(200)
+        expect(body.totalDocs).toBe(28)
+        expect(body.docsPerPage).toBe(10)
+        expect(body.totalPages).toBe(3)
+        expect(body.currentPage).toBe(1)
+        expect(body.nextPage).toBe(2)
+        expect(body.previousPage).toBeUndefined()
+        expect(body.data).toHaveLength(10)
+      })
+    })
   })
 })
