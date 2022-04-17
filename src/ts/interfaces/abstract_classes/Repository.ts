8078157130fd2fa685e44c505d0c5c8ebfca39bf
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-17 04:16:25 -0300

    feat: re-add countDocuments method to AbstractRepository

--- src/ts/interfaces/abstract_classes/Repository.ts
+++ src/ts/interfaces/abstract_classes/Repository.ts
@@ -1,6 +1,7 @@
 import { PaginateResult } from 'mongoose'
 
 export interface IRepository<T> {
+  countDocuments: () => Promise<number>
   create: (data: T) => Promise<T>
   read: (skip: number, limit: number) => Promise<PaginateResult<T>>
   update: (id: string, data: Partial<T>) => Promise<T | null>
