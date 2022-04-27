69d24edad47545355cf49db22d7c0b583905dbb4
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-27 18:23:43 -0300

    refactor: rename mongo connect function and logs

--- src/database/mongodb/connection.ts
+++ src/database/mongodb/connection.ts
@@ -2,18 +2,18 @@ import mongoose from 'mongoose'
 import { taskModel } from './models'
 import tasksSeed from './tasksSeed.json'
 
-export const connectToDatabase = async (): Promise<void> => {
+export const connectToMongoDB = async (): Promise<void> => {
   try {
     const mongoURI = process.env.MONGO_URI ?? 'mongodb://localhost:27017/mamboo-kanban-api'
     await mongoose.connect(mongoURI)
-    console.log('Database connection established')
+    console.log('MongoDB connection established')
     // If the tasks collection is empty it will be seeded with mock data during the first connection.
     if (await taskModel.countDocuments() === 0) {
       await taskModel.insertMany(tasksSeed)
-      console.log('Database seeded with 28 random tasks')
+      console.log('MongoDB seeded with 28 random tasks')
     }
   } catch (e) {
-    console.error('Database connection failed')
+    console.error('MongoDB connection failed')
     process.exit(1)
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import { connectToDatabase } from './database/mongodb'
+import { connectToMongoDB } from './database/mongodb'
 import { app } from './app'
 
 const PORT = process.env.PORT ?? 3001
 
 const start = async (): Promise<void> => {
-  await connectToDatabase()
+  await connectToMongoDB()
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
