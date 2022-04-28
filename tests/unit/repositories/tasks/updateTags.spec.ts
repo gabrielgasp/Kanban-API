80c33b62825155e3cd20b0827e2eaf36a394cacc
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-28 09:54:34 -0300

    test: adjust repositories updateTags.spec to reflect redis implementation

--- tests/unit/repositories/tasks/updateTags.spec.ts
+++ tests/unit/repositories/tasks/updateTags.spec.ts
@@ -1,8 +1,10 @@
+import Redis from "ioredis"
 import { taskModel } from "../../../../src/database/mongodb"
 import { TasksRepository } from "../../../../src/repositories/TasksRepository"
 
 const mockTasksModel = taskModel
-const tasksRepository = new TasksRepository(mockTasksModel)
+const mockRedis = {} as jest.Mocked<Redis>
+const tasksRepository = new TasksRepository(mockTasksModel, mockRedis)
 
 const fakeUpdatedTask = {
   _id: '1',
@@ -16,40 +18,73 @@ const fakeUpdatedTask = {
 }
 
 describe("TasksRepository updateTags method unit tests", () => {
-  beforeAll(() => { // mock the return value of the taskModel.findByIdAndUpdate method
-    mockTasksModel.findByIdAndUpdate = jest.fn()
-      .mockResolvedValueOnce({ ...fakeUpdatedTask, tags: ['test'] }) // mock the first call
-      .mockResolvedValueOnce({ ...fakeUpdatedTask, tags: ['test'] }) // mock the second call
-      .mockResolvedValueOnce({ ...fakeUpdatedTask, tags: [] }) // mock the third call
-      .mockResolvedValueOnce({ ...fakeUpdatedTask, tags: [] }) // mock the fourth call
-
+  beforeAll(() => { // mock the return value of the taskModel.modelName property
+    mockTasksModel.modelName = 'Task'
   })
 
   describe('When operation is 1', () => {
-    it("should call the findByIdAndUpdate method of the model with id, $addToSet, value and option new: true", async () => {
-      await tasksRepository.updateTags('1', '$addToSet', 'test')
-  
+    it("should call the findByIdAndUpdate method of the model with id, $addToSet, value, option new: true and 'redis.keys' with correct pattern", async () => {
+      mockTasksModel.findByIdAndUpdate = jest.fn().mockResolvedValueOnce({ ...fakeUpdatedTask, tags: ['test'] })
+      mockRedis.keys = jest.fn().mockResolvedValueOnce([])
+
+      await tasksRepository.updateTags('1', "$addToSet", 'test')
+
+      expect(mockRedis.keys).toHaveBeenCalledWith('Task:*')
       expect(mockTasksModel.findByIdAndUpdate).toHaveBeenCalledWith('1', { $addToSet: { tags: 'test' } }, { new: true })
     })
 
-    it("should return the result of the model's findByIdAndUpdate method", async () => {
+    it("should return the result of the model's findByIdAndUpdate method and not call 'redis.del' if length is === 0", async () => {
+      mockTasksModel.findByIdAndUpdate = jest.fn().mockResolvedValueOnce({ ...fakeUpdatedTask, tags: ['test'] })
+      mockRedis.keys = jest.fn().mockResolvedValueOnce([])
+      mockRedis.del = jest.fn()
+
       const result = await tasksRepository.updateTags('1', '$addToSet', 'test')
   
+      expect(mockRedis.del).not.toHaveBeenCalled()
       expect(result).toEqual({ ...fakeUpdatedTask, tags: ['test'] })
+    })
+
+    it("Should call 'redis.del' method with keys array if redisKeys length is > 0", async () => {
+      mockTasksModel.findByIdAndUpdate = jest.fn().mockResolvedValueOnce({ ...fakeUpdatedTask, tags: ['test'] })
+      mockRedis.keys = jest.fn().mockResolvedValueOnce(['test'])
+      mockRedis.del = jest.fn().mockResolvedValueOnce(1)
+
+      await tasksRepository.updateTags('1', '$addToSet', 'test')
+
+      expect(mockRedis.del).toHaveBeenCalledWith(['test'])
     })
   })
 
   describe('When operation is -1', () => {
-    it("should call the findByIdAndUpdate method of the model with id, $pull, value and option new: true", async () => {
+    it("should call the findByIdAndUpdate method of the model with id, $pull, value, option new: true and 'redis.keys' with correct pattern", async () => {
+      mockTasksModel.findByIdAndUpdate = jest.fn().mockResolvedValueOnce({ ...fakeUpdatedTask, tags: [] })
+      mockRedis.keys = jest.fn().mockResolvedValueOnce([])
+
       await tasksRepository.updateTags('1', '$pull', 'test')
   
+      expect(mockRedis.keys).toHaveBeenCalledWith('Task:*')
       expect(mockTasksModel.findByIdAndUpdate).toHaveBeenCalledWith('1', { $pull: { tags: 'test' } }, { new: true })
     })
 
-    it("should return the result of the model's findByIdAndUpdate method", async () => {
+    it("should return the result of the model's findByIdAndUpdate method and not call 'redis.del' if length is === 0", async () => {
+      mockTasksModel.findByIdAndUpdate = jest.fn().mockResolvedValueOnce({ ...fakeUpdatedTask, tags: [] })
+      mockRedis.keys = jest.fn().mockResolvedValueOnce([])
+      mockRedis.del = jest.fn()
+
       const result = await tasksRepository.updateTags('1', '$pull', 'test')
-  
+
+      expect(mockRedis.del).not.toHaveBeenCalled()
       expect(result).toEqual({ ...fakeUpdatedTask, tags: [] })
+    })
+
+    it("Should call 'redis.del' method with keys array if redisKeys length is > 0", async () => {
+      mockTasksModel.findByIdAndUpdate = jest.fn().mockResolvedValueOnce({ ...fakeUpdatedTask, tags: [] })
+      mockRedis.keys = jest.fn().mockResolvedValueOnce(['test'])
+      mockRedis.del = jest.fn().mockResolvedValueOnce(1)
+
+      await tasksRepository.updateTags('1', '$pull', 'test')
+
+      expect(mockRedis.del).toHaveBeenCalledWith(['test'])
     })
   })
 })
