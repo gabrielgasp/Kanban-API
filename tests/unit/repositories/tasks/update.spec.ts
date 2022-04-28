8fff3d0393a170df1709f062d39bbb90825830b9
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-28 09:32:23 -0300

    test: adjust repositories update.spec to reflect redis implementation

--- tests/unit/repositories/tasks/update.spec.ts
+++ tests/unit/repositories/tasks/update.spec.ts
@@ -1,8 +1,10 @@
 import { taskModel } from "../../../../src/database/mongodb"
+import { redis } from "../../../../src/database/redis"
 import { TasksRepository } from "../../../../src/repositories/TasksRepository"
 
 const mockTasksModel = taskModel
-const tasksRepository = new TasksRepository(mockTasksModel)
+const mockRedis = {} as jest.Mocked<typeof redis>
+const tasksRepository = new TasksRepository(mockTasksModel, mockRedis)
 
 const updateData = { status: 'in_progress' }
 
@@ -20,17 +22,34 @@ const fakeUpdatedTask = {
 describe("TasksRepository update method unit tests", () => {
   beforeAll(() => { // mock the return value of the taskModel.findByIdAndUpdate method
     mockTasksModel.findByIdAndUpdate = jest.fn().mockResolvedValue(fakeUpdatedTask)
+    mockTasksModel.modelName = 'Task'
   })
 
-  it("should call the findByIdAndUpdate method of the model with id, new values and option new: true", async () => {
+  it("should call the findByIdAndUpdate method of the model with id, new values, option new: true and 'redis.keys' with correct pattern", async () => {
+    mockRedis.keys = jest.fn().mockResolvedValueOnce([])
+
     await tasksRepository.update('1', updateData)
 
+    expect(mockRedis.keys).toHaveBeenCalledWith('Task:*')
     expect(mockTasksModel.findByIdAndUpdate).toHaveBeenCalledWith('1', updateData, { new: true })
   })
 
-  it("should return the result of the model's findByIdAndUpdate method", async () => {
+  it("should return the result of the model's findByIdAndUpdate method and not call 'redis.del' is length is === 0", async () => {
+    mockRedis.keys = jest.fn().mockResolvedValueOnce([])
+    mockRedis.del = jest.fn()
+
     const result = await tasksRepository.update('1', updateData)
 
+    expect(mockRedis.del).not.toHaveBeenCalled()
     expect(result).toEqual(fakeUpdatedTask)
+  })
+
+  it("Should call 'redis.del' method with keys array if redisKeys length is > 0", async () => {
+    mockRedis.keys = jest.fn().mockResolvedValueOnce(['test'])
+    mockRedis.del = jest.fn().mockResolvedValueOnce(1)
+
+    await tasksRepository.update('1', updateData)
+
+    expect(mockRedis.del).toHaveBeenCalledWith(['test'])
   })
 })
