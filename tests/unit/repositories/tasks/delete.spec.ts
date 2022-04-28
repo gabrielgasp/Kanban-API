2db1b826caa73869f61978e556bbc4da2388b639
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-28 09:35:46 -0300

    test: adjust repositories delete.spec to reflect redis implementation

--- tests/unit/repositories/tasks/delete.spec.ts
+++ tests/unit/repositories/tasks/delete.spec.ts
@@ -1,8 +1,10 @@
 import { taskModel } from "../../../../src/database/mongodb"
+import { redis } from "../../../../src/database/redis"
 import { TasksRepository } from "../../../../src/repositories/TasksRepository"
 
 const mockTasksModel = taskModel
-const tasksRepository = new TasksRepository(mockTasksModel)
+const mockRedis = {} as jest.Mocked<typeof redis>
+const tasksRepository = new TasksRepository(mockTasksModel, mockRedis)
 
 const fakeDeletedTask = {
   _id: '1',
@@ -18,17 +20,34 @@ const fakeDeletedTask = {
 describe("TasksRepository delete method unit tests", () => {
   beforeAll(() => { // mock the return value of the taskModel.findByIdAndDelete method
     mockTasksModel.findByIdAndDelete = jest.fn().mockResolvedValue(fakeDeletedTask)
+    mockTasksModel.modelName = 'Task'
   })
 
-  it("should call the findByIdAndDelete method of the model with no arguments", async () => {
+  it("should call the findByIdAndDelete method of the model with no arguments and 'redis.keys' with correct pattern", async () => {
+    mockRedis.keys = jest.fn().mockResolvedValueOnce([])
+
     await tasksRepository.delete('1')
 
+    expect(mockRedis.keys).toHaveBeenCalledWith('Task:*')
     expect(mockTasksModel.findByIdAndDelete).toHaveBeenCalledWith('1')
   })
 
-  it("should return the result of the model's findByIdAndDelete method", async () => {
+  it("should return the result of the model's findByIdAndDelete method and not call 'redis.del' if length is ==== 0", async () => {
+    mockRedis.keys = jest.fn().mockResolvedValueOnce([])
+    mockRedis.del = jest.fn()
+
     const result = await tasksRepository.delete('1')
 
+    expect(mockRedis.del).not.toHaveBeenCalled()
     expect(result).toEqual(fakeDeletedTask)
+  })
+
+  it("Should call 'redis.del' method with keys array if redisKeys length is > 0", async () => {
+    mockRedis.keys = jest.fn().mockResolvedValueOnce(['test'])
+    mockRedis.del = jest.fn().mockResolvedValueOnce(1)
+
+    await tasksRepository.delete('1')
+
+    expect(mockRedis.del).toHaveBeenCalledWith(['test'])
   })
 })
