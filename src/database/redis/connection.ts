cdff8de574b3c4bc4a85bb292cd6e147570d54b0
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-04-28 17:55:53 -0300

    refactor: move redis listeners to server.ts; simplify redis connection.ts

--- src/database/redis/connection.ts
+++ src/database/redis/connection.ts
@@ -1,16 +1,5 @@
 import Redis from 'ioredis'
 
-const redisOptions = process.env.REDIS_URL
-  ? { path: process.env.REDIS_URL } // used for heroku
-  : { host: process.env.REDIS_HOST } // used for docker-compose or defaults to 'localhost' if no env var is set.
-
-export const redis = new Redis(redisOptions)
-
-redis.on('connect', () => {
-  console.log('Redis connection established')
-})
-
-redis.on('error', () => { // If the connection fails, we exit the process.
-  console.error('Redis connection failed')
-  process.exit(1)
-})
+export const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL) // For heroku
+  : new Redis({ host: process.env.REDIS_HOST }) // For localhost (default) or docker-compose
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { connectToMongoDB } from './database/mongodb'
+import { redis } from './database/redis'
 import { app } from './app'
 
 const PORT = process.env.PORT ?? 3001
@@ -6,8 +7,16 @@ const PORT = process.env.PORT ?? 3001
 const start = async (): Promise<void> => {
   await connectToMongoDB()
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  redis.on('connect', () => { // If redis is connected, then start the server
+    console.log('Redis connection established')
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  })
+
+  redis.on('error', () => { // If the connection fails, we exit the process.
+    console.error('Redis connection failed')
+    process.exit(1)
   })
 }
 
